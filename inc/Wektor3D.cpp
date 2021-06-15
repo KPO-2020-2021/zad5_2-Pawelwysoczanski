ee715b08483a22aaacb1df86ac6a94549d5f3b53
Pawelwysoczanski <80000484+Pawelwysoczanski@users.noreply.github.com>
2021-06-15 12:12:14 +0200

    Delete Wektor3D.cpp

--- inc/Wektor3D.cpp
+++
@@ -1,9 +0,0 @@
-#include "Wektor3D.hh"
-
-
-/*
- *  Tutaj nalezy zdefiniowac odpowiednie metody
- *  klasy Wektor3D, ktore zawieraja wiecej kodu
- *  niz dwie linijki.
- *  Mniejsze metody mozna definiwac w ciele klasy.
- */
